5736e0cb92d5dd070d09d51dd2db59ccaf0c1c4b
HeHeHaHa-Home <see.teerasak@mail.com>
2016-05-18 01:27:58 +0700

    Lec 70 CustomView

---
+++ HelloWorld/app/src/main/java/com/hehehaha/helloworld/CustomView.java
@@ -0,0 +1,49 @@
+package com.hehehaha.helloworld;
+
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.os.Build;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by Teerasak on 5/18/2016.
+ */
+public class CustomView extends View {
+    public CustomView(Context context) {
+        super(context);
+        init();
+    }
+
+    public CustomView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    public CustomView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init();
+    }
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    public CustomView(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+        init();
+    }
+
+    private void init() {
+
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+        Paint p = new Paint();
+        p.setColor(0xFFFF0000); //#AARRGGBB
+
+        canvas.drawLine(0, 0, getMeasuredWidth(), getMeasuredHeight(), p);
+    }
+}
